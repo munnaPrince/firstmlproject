9b44281100535f215b684bef0777f5d392a86b72
munnaPrince <munnaf.prince.48@gmail.com>
2024-09-15 13:09:49 +0530

    model_trainer

--- SRC/components/data_ingestion.py
+++ SRC/components/data_ingestion.py
@@ -11,6 +11,9 @@ from dataclasses import dataclass
 from SRC.components.data_transformation import DataTransformation
 from SRC.components.data_transformation import DataTransformationConfig
 
+from SRC.components.model_trainer import ModelTrainerConfig
+from SRC.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifact',"train.csv")
@@ -52,6 +55,10 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modelTrainer = ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr,test_arr))
+
 
     
